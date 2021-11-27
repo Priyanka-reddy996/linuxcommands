f1655f5809276520913570fcfb3bc72bbde6c39d
priyanka <marthalapriyanka996@gmail.com>
2021-11-27 13:00:06 +0530

    new commit

---
+++ Day2 .java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+class Solution
+{
+ public static void main(String args[])
+ {
+  Scanner in=new Scanner(System.in);
+  System.out.println("Enter values:");
+  int t=in.nextInt();
+  for(int i=0;i<t;i++)
+  {
+   int a=in.nextInt();
+   int b=in.nextInt();
+   int n=in.nextInt();
+   int s=a+b;
+   System.out.println(s+" ");
+    for(int j=1;j<n;j++)
+   {
+    s+=((int)Math.pow(2,j))*b;
+    System.out.println(s+" ");
+   }
+    System.out.println();
+  }
+   in.close();
+ }
+}
+
+OUTPUT:1
+Enter values:
+2
+5 3 5
+8
+14
+26
+50
+98
+
+OUTPUT:2
+Enter values:
+2
+0 2 10
+2
+6
+14
+30
+62
+126
+254
+510
+1022
+2046
+
+
+
